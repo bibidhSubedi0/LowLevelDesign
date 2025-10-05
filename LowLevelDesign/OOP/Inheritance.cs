4171fddb6486fe3ce58517af4309cd32be6a7be4
Bibidh Subedi <080bct021.bibidh@pcampus.edu.np>
2025-10-05 11:23:55 +0545

    Inheritance

---
+++ LowLevelDesign/OOP/Inheritance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowLevelDesign.OOP
+{
+    internal class Inheritance
+    {
+    }
+}
+
+
+/*
+    Avoid inheritance if: The relationship is not genuinely “is-a” → prefer composition for “has-a” relationships.
+  
+           +----------------+
+           |   Vehicle      |  <-- Base class
+           +----------------+
+           | -speed: int    |
+           | +Start()       |
+           | +Stop()        |
+           | +Drive()       |
+           +----------------+
+                 ▲
+     ┌───────────┴─────────────┐
+     │                         │
++------------+          +------------+
+|   Car      |          |  Truck     |  <-- Derived classes
++------------+          +------------+
+| +Drive()   |          | +Drive()   |
++------------+          +------------+
+
+Inheritance doesn’t add algorithmic complexity; just a minor virtual-call dispatch at runtime.
+Abstract base class: Use if the base class should not be instantiated directly.
+
+
+Liskov Substitution Principle (L in SOLID): Subclasses should be usable in place of the base class without altering correctness.
+*/
